e4302cfaf1877fa505f64dbdc4e4751b00fbafe8
Gyubin-Han <yangglei2@naver.com>
2024-10-23 16:02:56 +0900

    first commit

---
+++ src/main/java/shop/S5G/bookApi/BookApiApplication.java
@@ -0,0 +1,13 @@
+package shop.S5G.bookApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApiApplication.class, args);
+	}
+
+}
